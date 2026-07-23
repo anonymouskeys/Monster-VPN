c257bb6f941afa3dbaa79faa285a871b445b31df
Anonymouskeys <admin@anonymouskeys.com>
2026-07-23 22:37:20 +0500

    Release v2.2.8

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.anonymouskeys.monstervpn"
         minSdk = 24
         targetSdk = 36
-        versionCode = 737
-        versionName = "2.2.7"
+        versionCode = 738
+        versionName = "2.2.8"
         multiDexEnabled = true
 
         // One universal APK containing every bundled ABI.
